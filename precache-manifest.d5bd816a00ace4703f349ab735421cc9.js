3b1a60a179b6b3c754fee82cae63ebd7e4f1e6de
mejba.13 <mejba.13@gmail.com>
2020-08-07 23:32:23 +0600

    Test commit

---
+++ precache-manifest.d5bd816a00ace4703f349ab735421cc9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e39a3ea7ada71c47b8c",
+    "url": "/static/css/main.be5bd52a.chunk.css"
+  },
+  {
+    "revision": "8e39a3ea7ada71c47b8c",
+    "url": "/static/js/main.26529429.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9a9f7787f875d006917",
+    "url": "/static/css/2.af237a14.chunk.css"
+  },
+  {
+    "revision": "b9a9f7787f875d006917",
+    "url": "/static/js/2.0027ce37.chunk.js"
+  },
+  {
+    "revision": "2703801579c52c71e481d8f2605a9bbe",
+    "url": "/static/media/shape1.27038015.svg"
+  },
+  {
+    "revision": "f044bf531a8b285e1fa239f03c3c335d",
+    "url": "/static/media/shape3.f044bf53.svg"
+  },
+  {
+    "revision": "78dc5ae61c6f2c97b51523cc2db8fc13",
+    "url": "/static/media/shape2.78dc5ae6.svg"
+  },
+  {
+    "revision": "d711f69562fb0c4045a23209c9006f76",
+    "url": "/static/media/map.d711f695.png"
+  },
+  {
+    "revision": "2c6110d15917655c4e774d02b10d31c9",
+    "url": "/static/media/blog-details.2c6110d1.jpg"
+  },
+  {
+    "revision": "075c0003ca0fd850587a2d5580641136",
+    "url": "/static/media/page-title-bg.075c0003.jpg"
+  },
+  {
+    "revision": "b1284fdb85a815ec161eb969a954822c",
+    "url": "/static/media/Flaticon.b1284fdb.eot"
+  },
+  {
+    "revision": "5bc0abcc78b7efab3e025335e48775f3",
+    "url": "/static/media/Flaticon.5bc0abcc.ttf"
+  },
+  {
+    "revision": "abb6fd71e7971d1f40edd4db65af4567",
+    "url": "/static/media/Flaticon.abb6fd71.svg"
+  },
+  {
+    "revision": "c78b6ecaad7e5e5fa9b5c13e8bd07a25",
+    "url": "/static/media/Flaticon.c78b6eca.woff"
+  },
+  {
+    "revision": "493f096bc43273f771ddcb1196f11882",
+    "url": "/static/media/Flaticon.493f096b.woff2"
+  },
+  {
+    "revision": "849465fc8c6b9a68c514be1d5bd24b6d",
+    "url": "/static/media/map2.849465fc.png"
+  },
+  {
+    "revision": "7c95f3279d968b373cef3aaa0488977d",
+    "url": "/static/media/footer-shape2.7c95f327.png"
+  },
+  {
+    "revision": "f9da81f80f22803b0f1926a81b181d70",
+    "url": "/static/media/project-details.f9da81f8.jpg"
+  },
+  {
+    "revision": "b5c1a479450d064886433152af9152e4",
+    "url": "/index.html"
+  }
+];
